79f857a0bc43b25b2fb229f0989f38c26e0d87a1
Valdemar <valdemar.kobelis@gmail.com>
2023-09-26 22:10:11 +0300

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :users
-  root to: "event#index"
+  root to: "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
