25d2a6b8ef5808648934609b310690c69af6216c
Valdemar <valdemar.kobelis@gmail.com>
2023-09-26 21:25:43 +0300

    Add actions to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(user_params)
+    @event.build_creator(current_user)
+    if @event.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:title, :date)
+  end
 end
