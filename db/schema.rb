1ee95d4f4e3234f63bdc5cdeabd879e12db9e85f
Valdemar <valdemar.kobelis@gmail.com>
2023-09-26 20:49:35 +0300

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_173235) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_174322) do
   create_table "events", force: :cascade do |t|
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "creator_id"
     t.string "title"
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_173235) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "creator_id"
 end
