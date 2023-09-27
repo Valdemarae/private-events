c167489d24b1de0c3bf88a13e355aec89de0a6e2
Valdemar <valdemar.kobelis@gmail.com>
2023-09-27 18:40:32 +0300

    Allow users to delete the events they created

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
 
   belongs_to :creator, class_name: "User"
 
-  has_many :event_attendees, foreign_key: "attended_event_id"
+  has_many :event_attendees, foreign_key: "attended_event_id", dependent: :destroy
   has_many :attendees, through: :event_attendees, source: :attendee
 
   validates :title, :presence => true
