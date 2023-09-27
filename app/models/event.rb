712d10aa8f036b5c9fd7814a1f162f62e26bcc18
Valdemar <valdemar.kobelis@gmail.com>
2023-09-27 15:12:52 +0300

    Create event model methods for quering past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
+  def self.past
+    where(:date => ...(Date.today))
+  end
+
+  def self.upcoming
+    where(:date => (Date.today)..)
+  end
+
   belongs_to :creator, class_name: "User"
 
   has_many :event_attendees, foreign_key: "attended_event_id"
